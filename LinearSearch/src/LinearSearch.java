371093acec9545887c9fd36fdb06008067dae811
wly_git <2825024660@qq.com>
2022-08-31 18:52:17 +0800

    LinearSearch

--- LinearSearch/src/LinearSearch.java
+++ LinearSearch/src/LinearSearch.java
@@ -4,4 +4,23 @@
  * @projectName Data Structures and Algorithms
  */
 public class LinearSearch {
+    public static int search(int[] data, int target){
+        for(int i = 0; i < data.length; i++){
+            if(data[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {24, 18, 12, 8, 9, 66, 32, 4};
+
+//        LinearSearch ls = new LinearSearch();
+        int res = LinearSearch.search(data, 66);
+        System.out.println(res);
+
+        int res2 = LinearSearch.search(data, 77);
+        System.out.println(res2);
+    }
 }
